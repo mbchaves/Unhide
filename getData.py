5935d04ff595e640b5263dd0fb2d0411f24ee9f5
mbchaves <48849454+mbchaves@users.noreply.github.com>
2024-04-07 14:12:45 -0300

    Atualizar o getData.py

--- getData.py
+++ getData.py
@@ -2,8 +2,20 @@ from bs4 import BeautifulSoup
 
 import requests
 
-html = requests.get("https://milhapremiada.com/reserva-manual/2/64993351742").content
+#url_get = "https://milhapremiada.com/reserva-manual/2/64993351742"
+#url_get ="https://milhapremiada.com/sorteio/3-mil-reais-no-pix?"
+#url_get = "https://milhapremiada.com/cadastra-reserva"
+#url_get = "https://milhapremiada.com/consultar-reserva"
+name = input('Qual a URL do site desejado?\n')
+dígitos= input('Quantos dígitos ?\n')
+primeira = input('Qual o número premiado?\n')
+numere= input('Qual o número  do bilhete?\n')
+url_get =  name
+html = requests.get(url_get).content
 
 soup = BeautifulSoup(html, 'html.parser')
 
 print(soup.prettify())
+
+temperatura = soup.find("table", class_="table")
+#print(temperatura)
